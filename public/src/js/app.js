d97ebdeb7c9f03bcc50d2ba53a1cbed1b2a35967
MoamenAbdelwahed <moomen.abdelwahed@gmail.com>
2016-07-14 17:56:18 +0200

    First commit

---
+++ public/src/js/app.js
@@ -0,0 +1,51 @@
+var postId = 0;
+var postBodyElement = null;
+
+$('.post').find('.interaction').find('.edit').on('click', function(){
+	event.preventDefault();
+
+	postBodyElement = event.target.parentNode.parentNode.childNodes[1];
+	var postBody = postBodyElement.textContent;
+	postId = event.target.parentNode.parentNode.dataset['postid'];
+	$('#post-body').val(postBody);
+	$('#edit-modal').modal('show');
+});
+
+$('#modal-save').on('click',function(){
+	$.ajax({
+		method: 'POST',
+		url: urlEdit,
+		data: {body: $('#post-body').val(), postId: postId, _token: token},
+		
+
+		error: function (xhr, ajaxOptions, thrownError) {
+	           console.log(xhr.status);
+	           console.log(xhr.responseText);
+	           console.log(thrownError);
+	       }
+	})
+	.done(function(msg){
+		$(postBodyElement).text(msg['new_body']);
+		$('#edit-modal').modal('hide');
+	});
+});
+
+$('.like').on('click', function(event){
+	event.preventDefault();
+	var isLike = event.target.previousElementSibling == null;
+	var	postId = event.target.parentNode.parentNode.dataset['postid'];
+	$.ajax({
+		method: 'POST',
+		url: urlLike,
+		data: {isLike: isLike, postId: postId, _token: token},
+	})
+	.done(function(){
+		event.target.innerText = isLike ? event.target.innerText == 'Like' ? 'You like this post' : 'Like' : event.target.innerText == 'Dislike' ? 'You don\'t like this post' : 'Dislike';
+		if(isLike){
+			event.target.nextElementSibling.innerText = 'Dislike';
+		}else{
+			event.target.previousElementSibling.innerText = 'Like';
+		}
+	});
+	
+});
